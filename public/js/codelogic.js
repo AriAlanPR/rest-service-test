5212fc2df95cc5dbc3283bd09478db81f870daa7
AriAlanPR <shaymin2003@gmail.com>
2022-02-14 17:06:15 -0600

    code fix

--- public/js/codelogic.js
+++ public/js/codelogic.js
@@ -1,1 +1,10 @@
 console.log("Entered code logic");
+
+function showTxtPrettyJSON() {
+    try {
+        let json = document.getElementById("body");
+        console.log("json", json);
+    } catch(e) {
+        console.error("Error converting text to Json");
+    }
+}
