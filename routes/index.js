adf215ba79425e6534babbdb452bfb05e10ec10d
AriAlanPR <shaymin2003@gmail.com>
2022-02-14 15:35:30 -0600

    test ws apis request content

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    console.log(req.query);
+
+    res.render('index', {
+        title: 'Test Node REST Services View',
+        data: JSON.stringify(req.query)
+    });
+});
+
+router.post('/', function(req, res){
+    console.log(req.body);
+
+    res.json(req.body);
+});
+
+router.get('/xmldata', function(req, res, next) {
+    
+});
+
+module.exports = router;
