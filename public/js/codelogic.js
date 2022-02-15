95d12acbf561083de78c30aa2e92706dd17a1080
AriAlanPR <shaymin2003@gmail.com>
2022-02-15 10:09:57 -0600

    added iframe and div for content

--- public/js/codelogic.js
+++ public/js/codelogic.js
@@ -6,6 +6,7 @@ function showTxtPrettyJSON() {
         json = JSON.parse(json);
         console.log("json: ");
         console.log(json);
+        document.getElementById("jsoncontent").innerHTML = json;
     } catch(e) {
         console.error("Error converting text to Json");
     }
