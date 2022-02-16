ec427125123bcf31bf0181c40403faaf82eb6729
AriAlanPR <shaymin2003@gmail.com>
2022-02-16 10:43:56 -0600

    added cors acceptance

--- public/js/codelogic.js
+++ public/js/codelogic.js
@@ -16,22 +16,23 @@ function showTxtPrettyJSON() {
 
 async function Request() {
     try {
-        let json = document.getElementById("json").value;
-        json = JSON.parse(json);
-        let url = document.getElementById("requesturl").value;
-        let method = document.getElementById("method").value;
+        let json = JSON.parse(document.getElementById("json").value);
+        json.body = json;
+        json.url = document.getElementById("requesturl").value;
+        json.method = document.getElementById("method").value;
+        
 
-        let response = await fetch(url, {
-            body: json,
+        let response = await fetch('/', {
+            body: JSON.stringify(json),
             method: method,
             headers: { 
                 "Content-Type": "application/json"
             }
         });
 
-        response = await response.text();
+        response = await response.json();
 
-        document.getElementById("jsoncontent").innerHTML = response;
+        document.getElementById("jsoncontent").innerHTML = JSON.stringify(response);
     } catch (e) {
         console.error("Error in request", e.message);
     }
--- routes/prettyprint.js
+++ routes/prettyprint.js
@@ -1,19 +1,35 @@
+
+var fetch = require('node-fetch');
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     console.log(req.query);
-
+    
     res.render('prettyprint', {
         title: 'Pretty printer view',
         data: JSON.stringify(req.query)
     });
 });
 
-router.post('/', function(req, res){
+router.post('/', async function(req, res){
     console.log(req.body);
+    let body = req.body;
 
-    res.json(req.body);
+    let json = body.body;
+
+    let response = await fetch(body.url, {
+        body: json,
+        method: body.method,
+        headers: { 
+            "Content-Type": "application/json"
+        }
+    });
+
+    response = await response.json();
+
+    res.json(response);
 });
 
 module.exports = router;
